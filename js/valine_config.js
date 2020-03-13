152d5d5f57acc9f498973d7030b26c38385a6b2f
ChrAlpha <noneworthy@outlook.com>
2020-03-13 16:01:11 +0800

    Update valine_config

---
+++ js/valine_config.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var valine = new Valine();
+valine.init({
+    el: '#valine_container',
+    notify: true,
+    verify: true,
+    appId: "FrtF1M9A3nUlChnLmbJX9v18-MdYXbMMI",
+    appKey: "ys9ICHGqYfDlPhwk5VPNHjDk",
+    placeholder: "填写邮箱方便收到动态通知哦~",
+    pageSize:'10',
+    avatar:'',
+    lang:'zh-cn',
+    visitor: 'false',
+     highlight:'true'
+});
