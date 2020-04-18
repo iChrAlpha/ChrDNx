21ccad0f29d83a0de0e66e17c913cf99f6c25dad
iChrAlpha <noneworthy@outlook.com>
2020-04-18 11:44:03 +0800

    perf(JS: lazydisqus & disqusJS): JS async lazyload

---
+++ js/disqusJS.js
@@ -0,0 +1,14 @@
+window.disqus_config = function () {
+    this.page.url = disqus_page_url;
+    this.page.identifier = disqus_page_identifier;
+}
+new DisqusJS({
+    shortname: 'chralpha',
+    siteName: 'ChrAlpha 的幻想乡',
+    api: 'https://disqus.skk.moe/disqus/',
+    apikey: 'a18bsKZH3l4wu9IV7RQpjdQPWaiFOeeW1HKFIXDhP6gXK39W84memwrq6hLN2Ipx',
+    admin: 'chralpha',
+    identifier: disqus_page_identifier,
+    url: disqus_page_url,
+    nesting: 2
+});
--- js/lazydisqus.js
+++ js/lazydisqus.js
@@ -1,23 +1,40 @@
-function loadDisqus() {
-  var $$ = document;
-  var s = $$.createElement('script');
-  s.src = 'https://chralpha.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  ($$.head || $$.body).appendChild(s);
-  window.disqus_config = function () {
-    this.page.url = disqus_page_url;
-    this.page.identifier = disqus_page_identifier;
-  }
-  new DisqusJS({
-    shortname: 'chralpha',
-    siteName: 'ChrAlpha 的幻想乡',
-    api: 'https://disqus.skk.moe/disqus/',
-    apikey: 'a18bsKZH3l4wu9IV7RQpjdQPWaiFOeeW1HKFIXDhP6gXK39W84memwrq6hLN2Ipx',
-    admin: 'chralpha',
-    identifier: disqus_page_identifier,
-    url: disqus_page_url,
-    nesting: 2
-  });
+async function addStyle(url) {
+    var d = document.createElement('link');
+    d.rel = 'stylesheet';
+    d.href = url;
+    d.async = true;
+    document.head.appendChild(d);
+}
+
+async function addScript(url) {
+    var d = document.createElement('script');
+    d.src = url;
+    d.async = true;
+    document.body.appendChild(d);
+}
+
+async function mainLoading() {
+    window.disqus_config = function () {
+        this.page.url = disqus_page_url;
+        this.page.identifier = disqus_page_identifier;
+    }
+    await new DisqusJS({
+        shortname: 'chralpha',
+        siteName: 'ChrAlpha 的幻想乡',
+        api: 'https://disqus.skk.moe/disqus/',
+        apikey: 'a18bsKZH3l4wu9IV7RQpjdQPWaiFOeeW1HKFIXDhP6gXK39W84memwrq6hLN2Ipx',
+        admin: 'chralpha',
+        identifier: disqus_page_identifier,
+        url: disqus_page_url,
+        nesting: 2
+    });
+}
+
+async function loadDisqus() {
+    await addScript('https://cdn.jsdelivr.net/npm/disqusjs@1.2/dist/disqus.js');
+    await addStyle('https://cdn.jsdelivr.net/npm/disqusjs@1.2.0/dist/disqusjs.css');
+    // await addScript('https://chralpha.disqus.com/embed.js');
+    await addScript('https://cdn.jsdelivr.net/npm/chrdnx@1.4.1/js/disqusJS.js');
 }
 
 
@@ -25,16 +42,16 @@ var runningOnBrowser = typeof window !== "undefined";
 var isBot = runningOnBrowser && !("onscroll" in window) || typeof navigator !== "undefined" && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent);
 var supportsIntersectionObserver = runningOnBrowser && "IntersectionObserver" in window;
 
-setTimeout(function () {
-  if (!isBot && supportsIntersectionObserver) {
-    var disqus_observer = new IntersectionObserver(function(entries) {
-      if (entries[0].isIntersecting) {
+setTimeout(async function () {
+    if (!isBot && supportsIntersectionObserver) {
+        var disqus_observer = new IntersectionObserver(function(entries) {
+        if (entries[0].isIntersecting) {
+            loadDisqus();
+            disqus_observer.disconnect();
+        }
+        });
+        disqus_observer.observe(document.getElementById('disqus_thread'));
+    } else {
         loadDisqus();
-        disqus_observer.disconnect();
-      }
-    });
-    disqus_observer.observe(document.getElementById('disqus_thread'));
-  } else {
-    loadDisqus();
-  }
+    }
 }, 1);
