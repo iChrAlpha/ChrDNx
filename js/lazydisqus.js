de91061a973912c1df9966b509da2bb153c1a04f
iChrAlpha <noneworthy@outlook.com>
2020-05-04 23:01:16 +0800

    fix(lazy disqusJS): api domain

--- js/lazydisqus.js
+++ js/lazydisqus.js
@@ -2,14 +2,13 @@ async function addStyle(url) {
     var d = document.createElement('link');
     d.rel = 'stylesheet';
     d.href = url;
-    d.async = true;
     document.head.appendChild(d);
 }
 
 async function addScript(url) {
     var d = document.createElement('script');
     d.src = url;
-    d.async = true;
+    d.async = false;
     document.body.appendChild(d);
 }
 
@@ -21,7 +20,7 @@ async function mainLoading() {
     await new DisqusJS({
         shortname: 'chralpha',
         siteName: 'ChrAlpha 的幻想乡',
-        api: 'https://disqus.skk.moe/disqus/',
+        api: 'https://dc.1amblog.ml/',
         apikey: 'a18bsKZH3l4wu9IV7RQpjdQPWaiFOeeW1HKFIXDhP6gXK39W84memwrq6hLN2Ipx',
         admin: 'chralpha',
         identifier: disqus_page_identifier,
@@ -32,9 +31,8 @@ async function mainLoading() {
 
 async function loadDisqus() {
     await addScript('https://cdn.jsdelivr.net/npm/disqusjs@1.2/dist/disqus.js');
-    await addStyle('https://cdn.jsdelivr.net/npm/disqusjs@1.2.0/dist/disqusjs.css');
-    // await addScript('https://chralpha.disqus.com/embed.js');
-    await addScript('https://cdn.jsdelivr.net/npm/chrdnx@1.4.1/js/disqusJS.js');
+    await addStyle('https://cdn.jsdelivr.net/npm/disqusjs@1.2/dist/disqusjs.css');
+    await addScript('https://cdn.jsdelivr.net/npm/chrdnx@1.4.2/js/disqusJS.js');
 }
 
 
@@ -45,10 +43,10 @@ var supportsIntersectionObserver = runningOnBrowser && "IntersectionObserver" in
 setTimeout(async function () {
     if (!isBot && supportsIntersectionObserver) {
         var disqus_observer = new IntersectionObserver(function(entries) {
-        if (entries[0].isIntersecting) {
-            loadDisqus();
-            disqus_observer.disconnect();
-        }
+            if (entries[0].isIntersecting) {
+                loadDisqus();
+                disqus_observer.disconnect();
+            }
         });
         disqus_observer.observe(document.getElementById('disqus_thread'));
     } else {
